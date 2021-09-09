c51304fde279cf043972d8998eb0c19ccee2f475
Rares Matei <matei.rar@gmail.com>
2021-09-09 11:35:07 +0100

    add chakra ui

--- apps/pizza-store/src/main.tsx
+++ apps/pizza-store/src/main.tsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
-
 import App from './app/app';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.render(
   <StrictMode>
+    <ChakraProvider>
     <App />
+    </ChakraProvider>
   </StrictMode>,
   document.getElementById('root')
 );
